304e22a08648873a09742d3893e71d57a2ec4019
Abhi-1U <PerricoQ@outlook.com>
2021-03-16 23:11:39 +0530

    added stack,force gc

--- src/controller/memory.py
+++ src/controller/memory.py
@@ -8,3 +8,25 @@ def heap_status():
         print(e)
     finally:
         gc.collect()
+
+def stack_status():
+    try:
+        import micropython
+        used_stack=micropython.stack_use()
+        free_stack=8192-used_stack
+        data={'used':used_stack,'free':free_stack,'total':8192}
+        return data
+    except Exception as e:
+        print(e)
+    finally:
+        gc.collect()
+
+def force_gc():
+    try:
+        from controller.success import success_code
+        gc.collect()
+        return success_code('OK_GC_1')
+    except Exception as e:
+        print(e)
+    finally:
+        gc.collect()
