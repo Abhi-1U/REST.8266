24ea33ae669e95e9442ac88584ae3f299a481e05
Abhi-1U <PerricoQ@outlook.com>
2021-03-19 23:50:00 +0530

    updated success codes.

--- src/controller/success.py
+++ src/controller/success.py
@@ -2,9 +2,12 @@ import gc
 
 def success_code(succcode):
     succcodes={
-        'OK_FS_1':'Resource Folder Created Successfully',
-        'OK_FS_2':'Resource Folder Removed Successfully',
-        'OK_GC_1':'Garbage Collector Initiated Successfully'
+        'OK_FS_1':'Resource Folder Created',
+        'OK_FS_2':'Resource Folder Removed',
+        'OK_FS_3': 'Working Directory Changed',
+        'OK_FS_4': 'File/Folder Renamed',
+        'OK_FS_5': 'File Removed',
+        'OK_GC_1':'Garbage Collector Initiated'
     }
     data={succcode:succcodes[succcode]}
     return data
