1c5ed0a82a43c94cd80fe194997775748ca5aaed
Abhi-1U <PerricoQ@outlook.com>
2021-03-26 22:26:35 +0530

    Added Method Error Code

--- src/controller/error.py
+++ src/controller/error.py
@@ -3,6 +3,8 @@ import gc
 def raise_error(errorcode):
     errorcodes={
         'ERR_FS_1':'File Path Not Valid'
+        'ERR_FS_2':'Access Denied',
+        'ERR_ME_1':'Unsupported HTTP Method' 
     }
     data={errorcode:errorcodes[errorcode]}
     return data
