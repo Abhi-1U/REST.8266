091cf9bfdeca6d28b2e2b97ffa0ad963c16ea237
Abhi-1U <PerricoQ@outlook.com>
2021-03-28 09:54:36 +0530

    updated methods

--- src/controller/gpio.py
+++ src/controller/gpio.py
@@ -16,14 +16,15 @@ def adc_read():
 
 def gpio_config(pin_no,pin_mode,setup):
     """
-    Set Pin as GPIO IN/OUT
+    Config GPIO Pin
     """
     try:
         from machine import Pin
+        from controller.success import success_code
         pin_no=int(pin_no)
         if pin_mode=='IN':
             if setup=='EPUR':
-                pin=Pin(int(pin_no),Pin.IN,Pin.PULL_UP)
+                pin=Pin(pin_no,Pin.IN,Pin.PULL_UP)
                 value={'P-val':pin.value()}
                 return value
             if setup=='EPDR':
@@ -35,8 +36,51 @@ def gpio_config(pin_no,pin_mode,setup):
                 value={'P-val':pin.value()}
                 return value
         if pin_mode=='OUT':
-            pass
+            if setup=='HIGH':
+                pin=Pin(int(pin_no),Pin.OUT,value=1)
+                return success_code('OK_IO_1')
+            if setup=='LOW':
+                pin=Pin(pin_no,Pin.OUT,value=0)
+                return success_code('OK_IO_1')
+            else:
+                pin=Pin(pin_no,Pin.OUT,value=1)
+                return success_code('OK_IO_1')
         return value
+    except Exception as e:
+        print(e)
+    finally:
+        gc.collect()
+def pwm(pin,freq,duty_cycle):
+    """
+    Set Up PWM
+    """
+    try:
+        from controller.success import success_code
+        from machine import Pin, PWM
+        pin=int(pin)
+        freq=int(freq)
+        duty_cycle=int(duty_cycle)
+        check_pin(pin_no,mode='PWM')
+        check_freq(freq)
+        check_dc(duty_cycle)
+        pwm2 = PWM(Pin(pin), freq=freq, duty=duty)
+        return success_code('OK_IO_3')
+    except Exception as e:
+        print(e)
+    finally:
+        gc.collect()
+
+def deinit_pwm(pin):
+    """
+    De-init PWM
+    """
+    try:
+        from controller.success import success_code
+        from machine import Pin, PWM
+        pin=int(pin)
+        pwm2 = PWM(Pin(pin))
+        pwm2.deinit()
+        return success_code('OK_IO_4')
     except Exception as e:
         print(e)
     finally:
