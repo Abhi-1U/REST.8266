fa51506260dd8a0322b7902c763163a8adecc7e1
Abhi-1U <PerricoQ@outlook.com>
2021-03-28 09:55:10 +0530

    success codes

--- src/controller/success.py
+++ src/controller/success.py
@@ -7,7 +7,12 @@ def success_code(succcode):
         'OK_FS_3': 'Working Directory Changed',
         'OK_FS_4': 'File/Folder Renamed',
         'OK_FS_5': 'File Removed',
-        'OK_GC_1':'Garbage Collector Initiated'
+        'OK_GC_1':'Garbage Collector Initiated',
+        'OK_IO_1':'GPIO Pin Initialized',
+        'OK_IO_3':'PWM Initialized',
+        'OK_IO_4':'PWM Deinitalized',
+        'OK_WR_1':'WebREPL Initialized',
+        'OK_WR_2':'WebREPL Code Changed'
     }
     data={succcode:succcodes[succcode]}
     return data
